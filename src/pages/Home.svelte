8fa157768e381b57aaae85e12282e70e64478545
Cuang Shulhan <cuangshulhan@gmail.com>
2021-05-06 13:41:27 +0700

    add pages and components

---
+++ src/pages/Home.svelte
@@ -0,0 +1,27 @@
+<script>
+	import CharityList from '../components/CharityList.svelte';
+
+	let title = "Charity";
+	let charities = ['Charity 1','Charity 2','Charity 3','Charity 4'];
+
+</script>
+
+<style>
+	h1{
+		font-size: 3em;
+		color: #e2071c;
+	}
+</style>
+
+<!-- <svelte:head>
+	<link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" />
+</svelte:head> -->
+
+<div>
+	<h1>{title}</h1>
+	<CharityList {charities}/>
+    <CharityList />
+</div>
+
+
+
