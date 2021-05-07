2d6f36b46e3f55fe16dd77d35c2167835a7728f5
Cuang Shulhan <cuangshulhan@gmail.com>
2021-05-07 14:27:13 +0700

    Convert HTML to svelte component

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail:
+        "https://image.freepik.com/free-vector/abstract-geometric-shapes-background_52683-60876.jpg",
+      pledged: 99000,
+      target: 100000,
+      date_end: +new Date("10 August 2021"),
+      profile_photo:
+        "https://image.freepik.com/free-vector/abstract-geometric-shapes-background_52683-60876.jpg",
+      profile_name: "Viroide Buneo",
+      no_pledges: 0,
+    },
+  ],
+};
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,27 +1,21 @@
 <script>
 	import CharityList from '../components/CharityList.svelte';
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
+	import Promo from '../components/Promo.svelte';
+	import Welcome from '../components/Welcome.svelte';
+	import { charities } from '../data/charities.js';
 
 	let title = "Charity";
-	let charities = ['Charity 1','Charity 2','Charity 3','Charity 4'];
 
 </script>
 
-<style>
-	h1{
-		font-size: 3em;
-		color: #e2071c;
-	}
-</style>
+<Header />
+<Welcome />
+<CharityList {charities}/>
+<Promo />
+<Footer />
 
-<!-- <svelte:head>
-	<link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" />
-</svelte:head> -->
-
-<div>
-	<h1>{title}</h1>
-	<CharityList {charities}/>
-    <CharityList />
-</div>
 
 
 
